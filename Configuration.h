b9f496b27b4120daba69a9eded43624d42753d33
Konstantin Polsky <k@polsky.ru>
2020-03-14 02:35:05 +0300

    pla preheat settings

--- Configuration.h
+++ Configuration.h
@@ -1461,8 +1461,8 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 180
-#define PREHEAT_1_TEMP_BED     70
+#define PREHEAT_1_TEMP_HOTEND 190
+#define PREHEAT_1_TEMP_BED     55
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
